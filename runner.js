bd101b8deb0dfffb2289e92234a0f9bf0bc989b6
Scott Burch <scott@bulldoginfo.com>
2015-01-26 13:48:50 -0800

    create sparrowHelper and start moving helpers out of runner.js

--- runner.js
+++ runner.js
@@ -199,40 +199,6 @@
                         return test();
                     }, done, 'waitUntilTrue: ' + test.toString());
                 },
-                wait: function (ms, done) {
-                    setTimeout(done, ms);
-                },
-                click: function (selector) {
-                    click();
-
-                    function click() {
-                        try {
-                            var elem = winVar.find(selector).get(0);
-                            var evt = winVar.get(0).createEvent("MouseEvents");
-                            var center_x = 1, center_y = 1;
-                            try {
-                                var pos = elem.getBoundingClientRect();
-                                center_x = Math.floor((pos.left + pos.right) / 2);
-                                center_y = Math.floor((pos.top + pos.bottom) / 2);
-                            } catch (e) {
-                            }
-                            evt.initMouseEvent('click', true, true, winVar.$window, 1, 1, 1, center_x, center_y, false, false, false, false, 0, elem);
-                            // dispatchEvent return value is false if at least one of the event
-                            // handlers which handled this event called preventDefault;
-                            // so we cannot returns this results as it cannot accurately informs on the status
-                            // of the operation
-                            // let's assume the event has been sent ok it didn't raise any error
-                            elem.dispatchEvent(evt);
-                            return true;
-                        } catch (e) {
-                            console.log("Failed dispatching click event on " + selector + ": " + e, "error");
-                            return false;
-                        }
-                    }
-                },
-                log: function (message) {
-                    console.log(message);
-                },
                 dump: function (selector, filename) {
                     selector = selector || 'html';
                     var html = winVar(selector).html();
@@ -247,14 +213,6 @@
                             alert(JSON.stringify(['writeFile', filename, s]));
                             console.log('captured: ' + selector);
                         }
-                    });
-                },
-                fill: function (selector, object) {
-                    var form = winVar.find(selector);
-                    _.each(object, function (value, name) {
-                        var field = winVar.find('[name="' + name + '"]', form);
-                        field.focus();
-                        field.val(value);
                     });
                 },
                 http: {
@@ -289,9 +247,7 @@
         var context = testsFrame ? testsFrame.get(0) : window;
         _.extend(context, {
             createTestWindow: function (winId) {
-                if (!testWindows[winId]) {
-                    testWindows[winId] = TestWin(winId);
-                }
+                testWindows[winId] || (testWindows[winId] = TestWin(winId));
             },
             sparrow: global.sparrow
 
